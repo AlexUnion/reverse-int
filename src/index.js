206979a3aad96c323dd20ab17b3ad8c75c2f0b3a
Al_chemick <alex2015mig@gmail.com>
2021-03-10 21:37:16 +0200

    feat: add reverse function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,6 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse (number) {
+    let str = number < 0 ?
+        String(number).substring(1) :
+        String(number);
+    return str.split('').reverse().join('');
 }
